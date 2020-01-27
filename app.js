56d8ea4e27aa1c1abfa507b0be2cdfb9cfd51db1
Ali Ghanem <alimhmdghanem@gmail.com>
2020-01-27 13:11:49 +0300

    Use Environment Variables to set the PORT

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+app.use(express.urlencoded({extended: true}))
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
+let campgrounds = [
+    {
+        name: "Campground 1",
+        image:
+            "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022__340.jpg"
+    },
+    {
+        name: "Campground 2",
+        image:
+            "https://cdn.pixabay.com/photo/2020/01/11/07/39/north-4756774__340.jpg"
+    },
+    {
+        name: "Campground 3",
+        image:
+            "https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201__340.jpg"
+    }
+];
+
+// Landing Page
 app.get("/", (req, res) => {
-    res.render("landing")
-})
+    res.render("landing");
+});
 
+// GET All Campgrounds
 app.get("/campgrounds", (req, res) => {
-    let campgrounds = [
-        {name: "Campground 1", image: "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022__340.jpg"},
-        {name: "Campground 2", image: "https://pixabay.com/get/57e1dd4a4350a514f6da8c7dda793f7f1636dfe2564c704c722672d69f4ac25d_340.jpg"},
-        {name: "Campground 3", image: "https://pixabay.com/get/57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c722672d69f4ac25d_340.jpg"}
-    ]
-    res.render("campgrounds", {campgrounds: campgrounds})
-})
+    res.render("campgrounds", { campgrounds: campgrounds });
+});
 
-app.listen(5500, "127.0.0.1", () => {
-    console.log("Server is running")
-})
+// Start the Server
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is running on ${port}`));
