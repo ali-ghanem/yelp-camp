7e93d81cb05ed41289695d2207c8b349ee843bb0
Ali Ghanem <alimhmdghanem@gmail.com>
2020-02-10 18:27:05 +0300

    ~ seeds.js: seed comments to campgrounds

--- seeds.js
+++ seeds.js
@@ -1,22 +1,23 @@
 const Campground = require("./models/campground");
+const Comment = require("./models/comment");
 
-const campgrounds = [
+const seeds = [
     {
-        name: "Campground 1",
+        name: "Camp 1",
         image:
             "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022__340.jpg",
         description:
             "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae veritatis quod fuga iusto laboriosam dolorem facilis eaque, quam molestiae nostrum!"
     },
     {
-        name: "Campground 2",
+        name: "Camp 2",
         image:
             "https://cdn.pixabay.com/photo/2020/01/11/07/39/north-4756774__340.jpg",
         description:
             "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae veritatis quod fuga iusto laboriosam dolorem facilis eaque, quam molestiae nostrum!"
     },
     {
-        name: "Campground 3",
+        name: "Camp 3",
         image:
             "https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201__340.jpg",
         description:
@@ -29,13 +30,26 @@ async function seedDB() {
         // remove all existing campgrounds
         await Campground.deleteMany({});
         console.log("Campgrounds removed");
-        
-        // add campgrounds
-        for(const camp of campgrounds){
-            await Campground.create(camp);
-            console.log(`Campground "${camp.name}" created`);
-        }
 
+        // remove all existing comments
+        await Comment.deleteMany({});
+        console.log("Comments removed");
+
+        // add campgrounds
+        for (const seed of seeds) {
+            let campground = await Campground.create(seed);
+            console.log(`Campground "${seed.name}" created`);
+
+            // create comment
+            let comment = await Comment.create({
+                text: "lorem ipsum .....",
+                author: "Ali"
+            });
+            // add comment to campground
+            campground.comments.push(comment);
+            campground.save();
+            console.log(`comment "${comment.text}" added to campground "${campground.name}"`);
+        }
     } catch (err) {
         console.log(err);
     }
