d38c8dfa48eae7beb6e1e5a89bb533fc25b6ce8d
Ali Ghanem <alimhmdghanem@gmail.com>
2020-02-13 11:51:26 +0300

    ~ seeds.js: stop seeding campgrounds

--- seeds.js
+++ seeds.js
@@ -1,29 +1,29 @@
 const Campground = require("./models/campground");
 const Comment = require("./models/comment");
 
-const seeds = [
-    {
-        name: "Camp 1",
-        image:
-            "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022__340.jpg",
-        description:
-            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae veritatis quod fuga iusto laboriosam dolorem facilis eaque, quam molestiae nostrum!"
-    },
-    {
-        name: "Camp 2",
-        image:
-            "https://cdn.pixabay.com/photo/2020/01/11/07/39/north-4756774__340.jpg",
-        description:
-            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae veritatis quod fuga iusto laboriosam dolorem facilis eaque, quam molestiae nostrum!"
-    },
-    {
-        name: "Camp 3",
-        image:
-            "https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201__340.jpg",
-        description:
-            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae veritatis quod fuga iusto laboriosam dolorem facilis eaque, quam molestiae nostrum!"
-    }
-];
+// const seeds = [
+//     {
+//         name: "Camp 1",
+//         image:
+//             "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022__340.jpg",
+//         description:
+//             "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae veritatis quod fuga iusto laboriosam dolorem facilis eaque, quam molestiae nostrum!"
+//     },
+//     {
+//         name: "Camp 2",
+//         image:
+//             "https://cdn.pixabay.com/photo/2020/01/11/07/39/north-4756774__340.jpg",
+//         description:
+//             "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae veritatis quod fuga iusto laboriosam dolorem facilis eaque, quam molestiae nostrum!"
+//     },
+//     {
+//         name: "Camp 3",
+//         image:
+//             "https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201__340.jpg",
+//         description:
+//             "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae veritatis quod fuga iusto laboriosam dolorem facilis eaque, quam molestiae nostrum!"
+//     }
+// ];
 
 async function seedDB() {
     try {
@@ -34,12 +34,6 @@ async function seedDB() {
         // remove all existing comments
         await Comment.deleteMany({});
         console.log("Comments removed");
-
-        // add campgrounds
-        for (const seed of seeds) {
-            await Campground.create(seed);
-            console.log(`Campground "${seed.name}" created`);
-        }
     } catch (err) {
         console.log(err);
     }
