717c26068e0aa8e86938aacda632cfba1869764f
Ali Ghanem <alimhmdghanem@gmail.com>
2020-02-18 23:55:22 +0300

    + routes/users.js

---
+++ routes/users.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const Campground = require("../models/campground");
+
+router.get("/", (req, res) => {
+    User.find({}, (err, users) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("users/index", { users });
+        }
+    });
+});
+
+router.get("/:id", (req, res) => {
+    User.findOne({ _id: req.params.id }, (err, user) => {
+        if (err) {
+            console.log(err);
+        } else {
+            Campground.find({}).where("author.id").equals(user._id).exec((err, campgrounds) => {
+
+                res.render("users/show", { user, campgrounds });
+            });
+        }
+    });
+});
+
+module.exports = router;
