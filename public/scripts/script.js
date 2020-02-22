41d48f24b5daabb26f810c55ca59341986104c7b
Ali Ghanem <alimhmdghanem@gmail.com>
2020-02-22 18:24:43 +0300

    ~ public/scripts/script.js: Add confirmation alert to .btn-delete

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,4 +2,8 @@ $(document).ready(() => {
     $("#deleteCampground").on("click", () =>
         confirm("Are you sure you want to delete this campground?")
     );
+
+    $(".btn-delete").on("click", () =>
+        confirm("Are you sure you want to delete it?")
+    );
 });
