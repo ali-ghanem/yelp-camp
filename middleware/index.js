d5fe0e855af9038b2408e74041aa94b860b76678
Ali Ghanem <alimhmdghanem@gmail.com>
2020-02-23 22:29:51 +0300

    ~ middleware/index.js: Handle errors in middlewares

--- middleware/index.js
+++ middleware/index.js
@@ -17,10 +17,10 @@ middlewareObj.isLoggedIn = (req, res, next) => {
 middlewareObj.isUser = (req, res, next) => {
     if (req.isAuthenticated()) {
         User.findOne({ _id: req.params.id }, (err, foundUser) => {
-            if (err) {
+            if (err || !foundUser) {
                 console.log(err);
                 req.flash("error", "User not found");
-                res.redirect("back");
+                res.redirect("/users");
             } else {
                 if (foundUser._id.equals(req.user._id)) {
                     next();
@@ -43,10 +43,10 @@ middlewareObj.isUser = (req, res, next) => {
 middlewareObj.isCampgroundAuthor = (req, res, next) => {
     if (req.isAuthenticated()) {
         Campground.findOne({ _id: req.params.id }, (err, foundCampground) => {
-            if (err) {
+            if (err || !foundCampground) {
                 console.log(err);
                 req.flash("error", "Campground not found");
-                res.redirect("back");
+                res.redirect("/campgrounds");
             } else {
                 if (foundCampground.author.equals(req.user._id)) {
                     next();
@@ -68,16 +68,31 @@ middlewareObj.isCampgroundAuthor = (req, res, next) => {
 // authorization middleware for comment
 middlewareObj.isCommentAuthor = (req, res, next) => {
     if (req.isAuthenticated()) {
-        Comment.findOne({ _id: req.params.comment_id }, (err, foundComment) => {
-            if (err) {
-                console.log(err);
-                res.redirect("back");
+        Campground.findOne({ _id: req.params._id }, (err, foundCampground) => {
+            if (err || !foundCampground) {
+                req.flash("error", "Campground not found");
+                res.redirect("/campgrounds");
             } else {
-                if (foundComment.author.equals(req.user._id)) {
-                    next();
-                } else {
-                    res.redirect("back");
-                }
+                Comment.findOne(
+                    { _id: req.params.comment_id },
+                    (err, foundComment) => {
+                        if (err || !foundComment) {
+                            console.log(err);
+                            req.flash("error", "Comment not found");
+                            res.redirect("/campgrounds/" + req.params.id);
+                        } else {
+                            if (foundComment.author.equals(req.user._id)) {
+                                next();
+                            } else {
+                                req.flash(
+                                    "error",
+                                    "You do not have permission to do that."
+                                );
+                                res.redirect("back");
+                            }
+                        }
+                    }
+                );
             }
         });
     } else {
