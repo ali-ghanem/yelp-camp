1b3b7bf8a3c2c13944d8d9b340048f60d70c59b1
Ali Ghanem <alimhmdghanem@gmail.com>
2020-02-25 22:51:48 +0300

    ~ public/scripts/script.js: Open/Close Sidebar listeners

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,13 +1,24 @@
 $(document).ready(() => {
-    $("#deleteUser").on("click", () =>
-        confirm("Are you sure you want to permanently delete your account?")
-    );
+    $("#deleteUser").on("click", function() {
+        confirm("Are you sure you want to permanently delete your account?");
+    });
 
-    $("#deleteCampground").on("click", () =>
-        confirm("Are you sure you want to delete this campground?")
-    );
+    $("#deleteCampground").on("click", function() {
+        confirm("Are you sure you want to delete this campground?");
+    });
 
-    $(".btn-delete").on("click", () =>
-        confirm("Are you sure you want to delete it?")
-    );
+    $(".btn-delete").on("click", function() {
+        confirm("Are you sure you want to delete it?");
+    });
+
+    // Campgrounds Index
+    $("#sidebarOpen").on("click", function() {
+        $(".sidebar").addClass("open");
+        $(".div-overlay").addClass("active");
+    });
+
+    $(".dismiss, .div-overlay").on("click", function() {
+        $(".sidebar").removeClass("open");
+        $(".div-overlay").removeClass("active");
+    });
 });
