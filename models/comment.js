6b8bfdcc6e8d3a5892d8ec784589f7cc6f2ffc24
Ali Ghanem <alimhmdghanem@gmail.com>
2020-02-28 18:17:24 +0300

    ~ models/comment.js: Add createdDate

--- models/comment.js
+++ models/comment.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const commentSchema = mongoose.Schema({
     text: String,
+    createdDate: { type: Date, default: Date.now },
     author: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
